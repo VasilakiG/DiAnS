2e3974213b712460ebeff8d09d02c9b60fb674ec
bdgToji <viktorbebek.vb@gmail.com>
2023-11-06 20:56:20 +0100

    Added pipe and filter lab1

---
+++ PipeAndFilter -vb/PipeAndFilter/src/main/java/service/impl/GradeFilter.java
@@ -0,0 +1,24 @@
+package service.impl;
+
+import service.Filter;
+
+import java.util.Arrays;
+
+public class GradeFilter implements Filter {
+    @Override
+    public String execute(Object input) {
+        String[] fields = input.toString().split(",");
+        String grade = fields[fields.length-1].toLowerCase();
+        if (grade.length()<3) {
+            int newGrade = Integer.parseInt(grade) - 5;
+            fields[fields.length-1] = String.valueOf(newGrade);
+        }
+
+        //Reconstruct the modified input
+        StringBuilder modifiedInput = new StringBuilder();
+        for(String field: fields){
+            modifiedInput.append(field).append(",");
+        }
+        return modifiedInput.substring(0,modifiedInput.length()-1);// Remove the trailing comma
+    }
+}
