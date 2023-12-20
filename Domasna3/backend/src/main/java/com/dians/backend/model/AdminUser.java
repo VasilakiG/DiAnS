d5a6ce60ca902b57fc6dac93d529a0bf6ca38598
Viktor Bebek <133436978+bdgToji@users.noreply.github.com>
2023-12-20 22:31:53 +0100

    Add files via upload
    FTR: added both User class and Admin class that implement the AppUse interface

---
+++ Domasna3/backend/src/main/java/com/dians/backend/model/AdminUser.java
@@ -0,0 +1,36 @@
+package com.dians.hw3.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "[adminUsers]")
+public class AdminUser implements AppUser{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String adminUsername;
+    private String adminPassword;
+    private String adminName;
+    private String adminSurname;
+
+    public AdminUser(){}
+
+    public AdminUser(String adminUsername, String adminPassword, String adminName, String adminSurname){
+        this.adminUsername=adminUsername;
+        this.adminPassword=adminPassword;
+        this.adminName=adminName;
+        this.adminSurname=adminSurname;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.adminUsername;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.adminPassword;
+    }
+}
---
+++ Domasna3/backend/src/main/java/com/dians/backend/model/User.java
@@ -0,0 +1,37 @@
+package com.dians.hw3.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "[users]")
+public class User implements AppUser{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String surname;
+
+    public User(){}
+
+    public User(String username, String password, String name, String surname){
+        this.id = (long) (Math.random()*1000);
+        this.username=username;
+        this.password=password;
+        this.name=name;
+        this.surname=surname;
+    }
+
+    @Override
+    public String getUsername(){
+        return this.username;
+    }
+
+    @Override
+    public String getPassword(){
+        return this.password;
+    }
+}
